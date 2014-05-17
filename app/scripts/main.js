db9c340d6e9698d303fa5840a4c1e6787686e97d
godspeedelbow <eelke@boezeman.com>
2014-05-18 00:29:49 +0200

    added some more dummy content and side scrolling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+    console.log('\'Allo \'Allo!');
+
+    $('#tell-me-more').on('click', function(e) {
+        $('html').addClass('side');
+        $('.pages').disablescroll();
+    });
+
+    $('#tell-me-less').on('click', function(e) {
+        $('html').removeClass('side');
+        $('.pages').disablescroll('undo');
+    });
+
+});
