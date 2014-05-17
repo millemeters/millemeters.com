f80d20e204b632505a3545db823ba20f07620584
godspeedelbow <eelke@boezeman.com>
2014-05-18 00:40:11 +0200

    used page id's as classes so we can reuse them

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
     console.log('\'Allo \'Allo!');
 
-    $('#tell-me-more').on('click', function(e) {
+    $('#tell-me-more').on('click', function() {
         $('html').addClass('side');
         $('.pages').disablescroll();
     });
 
-    $('#tell-me-less').on('click', function(e) {
+    $('#tell-me-less').on('click', function() {
         $('html').removeClass('side');
         $('.pages').disablescroll('undo');
     });
