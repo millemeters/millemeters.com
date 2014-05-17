637642fb53a10f9852e1a094136ba5ef4e3fe740
godspeedelbow <eelke@boezeman.com>
2014-05-18 01:50:35 +0200

    added scrolling plugin to scroll sideways, but it only works for scrolling away the side page now

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,26 @@
+function hideSidePage() {
+    $('html').removeClass('side');
+    $('.pages').removeClass('not-scrollable');
+}
+
+function showSidePage() {
+    $('html').addClass('side');
+    $('.pages').addClass('not-scrollable');
+}
+
 $(document).ready(function() {
     console.log('\'Allo \'Allo!');
 
-    $('.tell-me-more').on('click', function() {
-        $('html').addClass('side');
-        $('.pages').addClass('not-scrollable');
+    $('.tell-me-more').on('click', showSidePage);
+
+    $('.user-research-page').swipe({
+        swipeLeft: showSidePage
     });
 
-    $('.tell-me-less').on('click', function() {
-        $('html').removeClass('side');
-        $('.pages').removeClass('not-scrollable');
+    $('.tell-me-less').on('click', hideSidePage);
+
+    $('.side-page').swipe({
+        swipeRight: hideSidePage
     });
 
 });
