984dacfa7bcda7d1f6d8969751479c4f84c37ed1
godspeedelbow <eelke@boezeman.com>
2014-07-09 15:33:19 +0200

    added page overlay and made it clickable so that you can go back easier

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,13 +19,13 @@ function showSidePage(event) {
 function setupParallax() {
     $('.hero-page').each(function(){
         var $bgobj = $(this); // assigning the object
-             
+
         $('.pages').scroll(function() {
             var yPos = ($('.pages').scrollTop() / 2);
-             
+
             // Put together our final background position
             var coords = yPos + 'px';
- 
+
             // Move the background
             $bgobj.css({'background-position-y': coords });
         });
@@ -33,15 +33,13 @@ function setupParallax() {
 }
 
 function setupSidePages() {
-    console.log('\'Allo \'Allo!');
-
     $('.tell-me-more').on('click', showSidePage);
 
     $('.user-research-page').swipe({
         swipeLeft: showSidePage
     });
 
-    $('.tell-me-less').on('click', hideSidePage);
+    $('.tell-me-less, .overlay').on('click', hideSidePage);
 
     $('.side-page').swipe({
         swipeRight: hideSidePage
